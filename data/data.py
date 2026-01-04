bacb3f5b6413fca331f2df91c5ecfe518b21dd2b
Yashhhh69 <yashiknarayanan.2201262@srec.ac.in>
2026-01-04 12:26:57 +0530

    Update and rename data to data.py

---
+++ data/data.py
@@ -0,0 +1,14 @@
+data = {
+    'label': ['ham', 'spam', 'ham', 'spam', 'ham', 'spam', 'ham', 'spam'],
+    'message': [
+        'Hello, are we still meeting for coffee at 4?',
+        'WINNER! You have won a $1000 Walmart Gift Card. Call now!',
+        'Can you send me the files for the project?',
+        'URGENT: Your account has been compromised. Login here to fix.',
+        'Just checking in to see how you are doing.',
+        'FREE entry into our weekly contest. Text WIN to 80085.',
+        'I will be a bit late for the meeting, sorry.',
+        'Get a loan approved in 5 minutes! No credit check required.'
+    ]
+}
+df = pd.DataFrame(data)
